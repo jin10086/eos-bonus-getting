be70089ec7c6b9c096dd014e21e97cc0da6d6905
gaojin <igaojin@qq.com>
2018-10-04 00:25:24 +0800

    。

--- src/shishicai.py
+++ src/shishicai.py
@@ -5,29 +5,24 @@ from log import loggingSetting
 logger = loggingSetting("shishicai")
 
 
-def run():
+def run(ref):
     """https://lottery.eosplay.co/link"""
     accounts = getAccounts()
     for i in range(5, len(accounts) - 5):
         account = accounts[i]
-        if (
-            account != "gy2dgmztgqge"
-            or account != "acvnbzkffrsl"
-            or account != "bbheasxtlatd"
-        ):
-            if i % 2 == 0:
-                memo = "lottery:o@gy2dgmztgqge"
-            else:
-                memo = "lottery:e@gy2dgmztgqge"
-            t = pushaction(
-                "eosio.token",
-                "transfer",
-                [account, "eosplaybrand", "0.1000 EOS", memo],
-                account,
-            )
-            if b"transaction" not in t:
-                logger.info("转账失败:{}".format(account))
-                logger.info("原因为:{}".format(t))
+        if i % 2 == 0:
+            memo = "lottery:o@{}".format(ref)
+        else:
+            memo = "lottery:e@{}".format(ref)
+        t = pushaction(
+            "eosio.token",
+            "transfer",
+            [account, "eosplaybrand", "0.1000 EOS", memo],
+            account,
+        )
+        if b"transaction" not in t:
+            logger.info("转账失败:{}".format(account))
+            logger.info("原因为:{}".format(t))
 
 
-run()
+run("gy2dgmztgqge")
