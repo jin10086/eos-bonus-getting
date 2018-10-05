505459eb9e33b1dee7769ba412d211a0c83896b8
gaojin <igaojin@qq.com>
2018-10-05 20:55:55 +0800

    匿名创建账号

--- src/createAccount.py
+++ src/createAccount.py
@@ -1,4 +1,4 @@
-from ut import pushaction, runcleos
+from ut import pushaction, runcleos, delegatebw
 from log import loggingSetting
 import random
 import string
@@ -55,8 +55,26 @@ def createUser(publicKey, name=None, cpu=0.1, net=0.1, ram=3):
     logger.info(t)
 
 
+def createUser1(publicKey, f, _amount=0.64, name=None):
+    "使用 signupeoseos 来注册匿名账号,http://signupeoseos.com/#/"
+    if not name:
+        name = genrateRandomName()
+    # 直到随机到一个没有注册的用户名.
+    amount = "%.4f EOS" % _amount
+    while True:
+        if checkName(name):
+            break
+        name = genrateRandomName()
+    memo = "{}-{}".format(name, publicKey)
+    t = pushaction("eosio.token", "transfer", [f, "signupeoseos", amount, memo], f)
+
+    logger.info("account:{}".format(name))
+    logger.info(t)
+
+
 if __name__ == "__main__":
     logger.info("开始注册账号...")
-    for i in range(50):
-        publicKey = ""
-        createUser(publicKey)
+    publicKey = ""
+    f = ""
+    for i in range(100):
+        createUser1(publicKey, f)
