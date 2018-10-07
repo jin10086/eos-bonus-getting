a0c33a92bdcc0a9c0527d763637bbbc6620718a8
gaojin <igaojin@qq.com>
2018-10-07 10:01:56 +0800

    add EosRoyale

--- src/airdrop.py
+++ src/airdrop.py
@@ -9,16 +9,17 @@ logger = loggingSetting("airdrop")
 def main(password):
     x = getAccounts()
     unlock(password)
-    for i in x[:1]:
-        print(
-            pushaction("betdicetoken", "signup", [i, "1000.0000 DICE"], i)
-        )  # 1000dice
+    for i in x:
+        # print(
+        #     pushaction("betdicetoken", "signup", [i, "1000.0000 DICE"], i)
+        # )  # 1000dice
         # print(
         #     pushaction("xxxsevensxxx", "signup", [i, "10000.0000 SEVEN"], i) # 10000 SEVEN
         # )
         # print(pushaction("efinitysicbo", "claim", [i], i))  # 100 CHIPS
+        print(pushaction("roulettespin", "login", [i, "gy2dgmztgqge"], i))
 
 
 if __name__ == "__main__":
-    password = ""
+    password = "1"
     main(password)
