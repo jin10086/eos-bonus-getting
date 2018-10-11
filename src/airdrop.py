f42a47a772011c8bc93580c49e19177c97a7ba87
gaojin <igaojin@qq.com>
2018-10-11 23:48:29 +0800

    Update src/airdrop.py

--- src/airdrop.py
+++ src/airdrop.py
@@ -1,25 +1,39 @@
 import os
 import json
 from log import loggingSetting
-from ut import pushaction, unlock, getAccounts
+from ut import pushaction, unlock, getAccounts, runPool
 
 logger = loggingSetting("airdrop")
 
 
 def main(password):
-    x = getAccounts()
-    unlock(password)
-    for i in x:
-        # print(
-        #     pushaction("betdicetoken", "signup", [i, "1000.0000 DICE"], i)
-        # )  # 1000dice
-        # print(
-        #     pushaction("xxxsevensxxx", "signup", [i, "10000.0000 SEVEN"], i) # 10000 SEVEN
-        # )
-        # print(pushaction("efinitysicbo", "claim", [i], i))  # 100 CHIPS
-        print(pushaction("roulettespin", "login", [i, "gy2dgmztgqge"], i))
+    pass
+
+    # for i in x[1:]:
+    # print(pushaction("betdicetoken", "signup", [i, "1000.0000 DICE"], i))  # 1000dice
+    # print(
+    #     pushaction("xxxsevensxxx", "signup", [i, "10000.0000 SEVEN"], i)  # 10000 SEVEN
+    # )
+    # print(pushaction("efinitysicbo", "claim", [i], i))  # 100 CHIPS
+    # print(pushaction("roulettespin", "login", [i, "gy2dgmztgqge"], i))
+    # print(pushaction("efinitychips", "claim", [i, "gy2dgmztgqge"], i))
+    # print(pushaction("grandpacoins", "mine", [i, "4,BTC", "gy2dgmztgqge"], i))
+    # print(pushaction("grandpacoins", "mine", [i, "4,ETH", "gy2dgmztgqge"], i))
+    # print(pushaction("grandpacoins", "mine", [i, "4,DOGE", "gy2dgmztgqge"], i))
+    # print(pushaction("poormantoken", "signup", [i, "0.0000 POOR"], i))
+    # print(pushaction("trybenetwork", "claim", [i], i))
+    # print(pushaction("wizznetwork1", "signup", [i, "0.0000 WIZZ"], i))
 
 
 if __name__ == "__main__":
     password = "1"
-    main(password)
+    unlock(password)
+    accounts = getAccounts()
+
+    def run(i):
+        print(pushaction("poormantoken", "signup", [i, "0.0000 POOR"], i))
+        print(pushaction("trybenetwork", "claim", [i], i))
+        print(pushaction("wizznetwork1", "signup", [i, "0.0000 WIZZ"], i))
+
+    # runPool(run, accounts)
+    run("gy2dgmztgqge")
