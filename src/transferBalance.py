f3e24cb583b0932844e58e065153c0e064ac0d62
gaojin <igaojin@qq.com>
2018-10-11 15:24:24 +0800

    剪影 与 add

--- src/transferBalance.py
+++ src/transferBalance.py
@@ -14,6 +14,14 @@ def tranferDice(f, to, n, m=""):
     return pushaction("betdicetoken", "transfer", [f, to, "%.4f DICE" % n, m], f)
 
 
+def tranferAdd(f, to, n, m=""):
+    return pushaction("eosadddddddd", "transfer", [f, to, "%.4f ADD" % n, m], f)
+
+
+def tranferEsa(f, to, n, m=""):
+    return pushaction("shadowbanker", "transfer", [f, to, "%.4f ESA" % n, m], f)
+
+
 if __name__ == "__main__":
     accounts = getAccounts()
     for i in accounts:
