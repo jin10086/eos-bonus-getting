cfd9d6a070c3210ccb67231396ca764f38d4170b
jin10086 <1019855680@qq.com>
2018-12-14 23:54:27 +0800

    Update betsand.py

--- src/betsand.py
+++ src/betsand.py
@@ -7,10 +7,10 @@ import time
 
 
 def run():
-    f = "gy2dgmztgqge"
+    f = "gaojin.game"
     t = "betsandrou11"
 
-    memo = "jB5|jA5,df89268a3ab52962a2b58fa4f2ab65d1c1d0a99a,167a7764ab4,gaojin.game"
+    memo = "jB5|jA5,cd4c87e232a7ae0b0aa9697b13efb021c6ba9020,167a81a76e1,gy2dgmztgqge"
     print(transferEos(f, t, 1, memo))
 
 
@@ -20,5 +20,5 @@ def genrateRandomName():
 
 if __name__ == "__main__":
     while True:
-        time.sleep(0.51)
+        time.sleep(0.511)
         run()
