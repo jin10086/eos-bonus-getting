0a895e1419881b9f47325544da7d5ca0c379443e
jin10086 <1019855680@qq.com>
2019-01-05 19:56:07 +0800

    Update transferBalance.py

--- src/transferBalance.py
+++ src/transferBalance.py
@@ -1,8 +1,7 @@
 import pickle
 
 from log import loggingSetting
-from ut import pushaction, getAccounts
-
+from ut import pushaction
 logger = loggingSetting("transferBalance")
 
 
@@ -34,8 +33,20 @@ def tranferKar(f, to, n, m=""):
     return pushaction("therealkarma", "transfer", [f, to, "%.4f KARMA" % n, m], f)
 
 
+def tranferRich(f, to, n, m=""):
+    return pushaction("richrich1111", "transfer", [f, to, "%.4f RICH" % n, m], f)
+
+
 def tranferEUSD(f, to, n, m=""):
     return pushaction("bitpietokens", "transfer", [f, to, "%.8f EUSD" % n, m], f)
+
+
+def tranferEETH(f, to, n, m=""):
+    return pushaction("bitpietokens", "transfer", [f, to, "%.8f EETH" % n, m], f)
+
+
+def tranferEBTC(f, to, n, m=""):
+    return pushaction("bitpietokens", "transfer", [f, to, "%.8f EBTC" % n, m], f)
 
 
 def tranferJKS(f, to, n, m=""):
@@ -46,9 +57,13 @@ def tranferZks(f, to, n, m=""):
     return pushaction("zkstokensr4u", "transfer", [f, to, "%.4f ZKS" % n, m], f)
 
 
+def tranferRoy(f, to, n, m=""):
+    return pushaction("eosroyaleroy", "transfer", [f, to, "%.4f ROY" % n, m], f)
+
+
+def tranferBase(code, action, symbol, f, to, n, m="", decimal=4):
+    return pushaction(code, action, [f, to, f"%.{decimal}f {symbol}" % n, m], f)
+
+
 if __name__ == "__main__":
     accounts = getAccounts()
-    for i in accounts:
-        print(i)
-        t = tranferDice(i, "gy2dgmztgqge", 1000)
-        logger.info(t)
